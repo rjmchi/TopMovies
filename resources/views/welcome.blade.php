adefba33ac48382104aca2cf7934c6d920bd0f22
rjmchi <robert@rjmprogramming.com>
2025-12-04 01:42:47 -0600

    upgrade to Laravel 11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,13 @@
 <x-guest-layout>
-<h1 class="text-4xl bold text-center">Movie List</h1>
-@foreach ($categories as $cat)
-    <h2 class="text-3xl bold">{{$cat->name}}</h2>
-    @foreach($cat->movies as $movie)
-        <p class="ml-5">{{$movie->rank}} &mdash; {{$movie->title}}</p>
+    <h1 class="text-4xl text-center bold">Movie List</h1>
+    @foreach ($categories as $cat)
+        <h2 class="text-3xl bold">{{ $cat->name }}</h2>
+        @foreach ($cat->movies as $movie)
+            <p class="ml-5">{{ $movie->rank }} &mdash; {{ $movie->title }}</p>
+        @endforeach
     @endforeach
-@endforeach
+
+    <div class="ml-4 text-sm text-center text-gray-500 sm:text-right sm:ml-0">
+        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+    </div>
 </x-guest-layout>
